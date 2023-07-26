9da275b2c36f0bb457ae12774acaa1d173153420
Ahmed-Ebrahim03 <a.ebrahim03@outlook.com>
2023-07-27 00:01:29 +0300

    Updated1

---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.grey.withOpacity(0.1),
+      ),
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        constraints: const BoxConstraints(),
+        icon: const Icon(Icons.search),
+        onPressed: () {},
+      ),
+    );
+  }
+}
